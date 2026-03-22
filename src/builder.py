8bb5c957339243b6196cbd767b00277d0e8051f4
lithium <darrenchang1903@gmail.com>
2026-03-22 09:40:48 +0700

    modified builder.py

--- src/builder.py
+++ src/builder.py
@@ -1,7 +1,15 @@
 import requests
-import json
-from pathlib import Path
 from os import getenv, makedirs
+import questionary
+import json
+from rich.theme import Theme
+from rich.console import Console
+from pathlib import Path
+from sys import exit
+
+custom_theme = Theme({"error": "bold red", "success": "green", "warning": "yellow"})
+console = Console(theme=custom_theme, highlight=False)
+print = console.print
 
 
 def main():
@@ -15,8 +23,27 @@ def main():
     # if idslugmap.json exists: if you downloaded a new version of mods.json: redo creation
     # if doesnt exist: do creation
 
-    def get_mods_json():
-        pass
+    def get_mods_json() -> str:
+        etag_filepath = appdata_filepath / "mods.etag"
+        mods_url = "https://raw.githubusercontent.com/nerrader/nerraders-mc-mod-downloader/refs/heads/main/data/mods.json"
+        api_headers = {}
+        if etag_filepath.exists():
+            api_headers["If-None-Match"] = etag_filepath.read_text().strip()
+        response = requests.get(mods_url, headers=api_headers, timeout=5)
+        if response.status_code == 304:
+            return False
+        elif response.status_code != 200:
+            raise requests.exceptions.HTTPError(
+                f"CRITICAL ERROR: Could not connect to mod list, github API responded with status {response.status_code}"
+            )
+        data = response.json()
+
+        with open(mods_filepath, "w") as file:
+            json.dump(data, file, indent=4)
+        if "ETag" in response.headers:
+            with open(etag_filepath, "w") as file:
+                etag_filepath.write_text(response.headers["ETag"])
+        return True
 
     def get_slugslist() -> set[str]:  # only slugs, no ids
         """Summary:
@@ -32,6 +59,7 @@ def main():
                 category
             ) in modslist.values():  # for category in modslist, for mod in category
                 for mod in category:
+                    print(mod)
                     slugslist.add(mod["value"])
         return slugslist
 
@@ -48,9 +76,7 @@ def main():
         api_url = "https://api.modrinth.com/v2/projects"
         api_params = {"ids": json.dumps(slugslist)}
         response = requests.get(api_url, params=api_params)
-        if response.status_code != 200:
-            print("somethings up")
-            print(response.status_code)
+        response.raise_for_status()
         data = response.json()
         for mod in data:
             idslugmap[mod["id"]] = mod["slug"]  # for mod in data
@@ -60,11 +86,16 @@ def main():
     def get_slugsidmap():
         modify_slugsmap(get_slugslist())
 
-    if not mods_filepath.exists():
-        get_mods_json()
+    try:
+        should_update_idslugmap = (
+            get_mods_json()
+        )  # returns true if needs to be updated, false otherwise
 
-    if not idslugmap_filepath.exists():
-        get_slugsidmap()
+        if should_update_idslugmap or not idslugmap_filepath.exists():
+            get_slugsidmap()
+    except requests.exceptions.HTTPError as error:
+        print(f"Critical Error: {error}")
+        exit(1)
 
 
 if __name__ == "__main__":
